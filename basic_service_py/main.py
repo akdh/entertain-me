8168b19b75f0512a5725760be7e5eb6cba448c11
akdh <adeanhall@gmail.com>
2015-02-21 13:14:03 -0500

    Updated example service
    The example service is updated to align with recent API changes.

--- basic_service_py/main.py
+++ basic_service_py/main.py
@@ -1,29 +1,50 @@
 from flask import Flask, jsonify, request
-import json, random, requests
+import json, random, requests, click
 
 app = Flask(__name__)
 
-API_KEY = 'DEMOKEY'
-REGISTRATION_URL = 'http://127.0.0.1:5000/%s/registrations' % API_KEY
+API_BASE = 'http://localhost:3000/api'
 MAX_ATTRACTION_ID = 75010
 
-@app.route('/suggestions', methods=['GET', 'POST'])
+@app.route('/suggestions', methods=['POST'])
 def suggestions():
-    if request.method == 'GET':
-        return jsonify({'success': True})
-    elif request.method == 'POST':
-        data = request.get_json()
-        location = data['location']
-        profile = data['profile']
-        user_id = profile['id']
-        preferences = profile['preferences']
-        suggestions = [random.randrange(0, MAX_ATTRACTION_ID) for i in range(10)]
-        return jsonify({'suggestions': suggestions})
+    data = request.get_json()
+    location = data['locationId']
+    person = data['personId']
+    suggestions = [random.randrange(0, MAX_ATTRACTION_ID) for i in range(10)]
+    return jsonify({'suggestions': suggestions})
 
-@app.route('/register', methods=['GET'])
-def register():
-    r = requests.post(REGISTRATION_URL, params={'callback_url': 'http://127.0.0.1:5002/suggestions'})
-    return str(r.status_code)
+@click.group()
+def cli():
+    pass
+
+@cli.command()
+@click.argument('email')
+@click.argument('password')
+def create(email, password):
+    r = requests.post(API_BASE + '/services', data={'email': email, 'password': password})
+    print(r.status_code)
+    print(r.json())
+
+@cli.command()
+@click.argument('email')
+@click.argument('password')
+def login(email, password):
+    r = requests.post(API_BASE + '/services/login', data={'email': email, 'password': password})
+    print(r.status_code)
+    print(r.json())
+
+@cli.command()
+@click.argument('service_id')
+@click.argument('access_token')
+def register(service_id, access_token):
+    r = requests.post(API_BASE + '/services/%s/subscriptions?access_token=%s' % (service_id, access_token), data={'callback_url': 'http://127.0.0.1:5002/suggestions'})
+    print(r.status_code)
+    print(r.json())
+
+@cli.command()
+def server():
+    app.run(debug=True, threaded=True, port=5002)
 
 if __name__ == '__main__':
-    app.run(debug=True, threaded=True, port=5002)
+    cli()
