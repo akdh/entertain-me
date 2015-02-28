60acb7b2edfa1266b4a6d5c1685a3d05a498be6b
akdh <adeanhall@gmail.com>
2015-02-28 17:07:26 -0500

    Fixed to use latest preference in scoring

--- common/models/response.js
+++ common/models/response.js
@@ -4,7 +4,7 @@ module.exports = function(Response) {
     Response.prototype.score = function(cb) {
         var documentIds = this.body.suggestions;
         this.request(function(err, request) {
-            request.preferences({where: {personId: request.personId, documentId: {inq: documentIds}}}, function(err, preferences) {
+            request.preferences({where: {personId: request.personId, documentId: {inq: documentIds}}, order: 'updated DESC'}, function(err, preferences) {
                 var preferencesByDocumentId = _.groupBy(preferences, 'documentId');
                 var likedDocumentIds = _.filter(documentIds, function(documentId) {
                     return preferencesByDocumentId[documentId] && preferencesByDocumentId[documentId][0] && preferencesByDocumentId[documentId][0].rating === 5;
