3cb95e639e231498dd747bb001827158a1660bd1
akdh <adeanhall@gmail.com>
2015-03-13 16:21:19 -0400

    Added uniqueness check to preference table.
    Also removed table list from automigrate call.

--- server/create-lb-tables.js
+++ server/create-lb-tables.js
@@ -1,8 +1,8 @@
 var server = require('./server');
 var ds = server.dataSources.db;
-var lbTables = ['User', 'AccessToken', 'ACL', 'RoleMapping', 'Role', 'document', 'person', 'preference', 'subscription', 'location', 'service', 'request', 'response']
-ds.automigrate(lbTables, function(err) {
+ds.automigrate(function(err) {
     if(err) throw err;
+    ds.connector.query('CREATE UNIQUE INDEX documentId_personId_idx ON preference (documentId, personId) WHERE parentId IS NULL');
     console.log('Created.');
     ds.disconnect();
 })
