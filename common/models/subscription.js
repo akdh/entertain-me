d2936bb1627bf38f9f284fe75c6a8bb6f519903f
akdh <adeanhall@gmail.com>
2015-04-21 19:30:05 -0400

    Subscription creation now has test request to URL.

--- common/models/subscription.js
+++ common/models/subscription.js
@@ -1,3 +1,7 @@
+var request = require('request');
+var loopback = require('loopback');
+var async = require('async');
+
 module.exports = function(Subscription) {
     Subscription.observe('before delete', function(ctx, next) {
         Subscription.updateAll(ctx.where, {deleted: new Date()}, function(err, count) {
@@ -5,4 +9,42 @@ module.exports = function(Subscription) {
             next(err);
         })
     })
+
+    Subscription.observe('before save', function(ctx, next) {
+        var Person = loopback.getModel('person');
+        var Location = loopback.getModel('location');
+
+        if(ctx.instance) {
+            var url = ctx.instance.callback_url;
+        } else {
+            var url = ctx.data.callback_url;
+        }
+        async.parallel({
+            person: function(cb) {
+                Person.findOne({include: {relation: 'preferences', scope: {fields: ['rating', 'documentId']}}, fields: {id: true}}, cb);
+            },
+            location: function(cb) {
+                Location.findOne(cb);
+            }
+        }, function(err, results) {
+            if(err) {
+                cb(err, null);
+            } else if(results.person === null) {
+                cb("Invalid personId.", null);
+            } else if(results.location === null) {
+                cb("Invalide locationId.", null);
+            } else {
+                var data = {person: results.person, location: results.location};
+                request.post(url, {json: data}, function(err, response, body) {
+                    if(err) {
+                        next(err);
+                    }
+                    if(response.statusCode != 200) {
+                        next("HTTP status code must be 200, it was: " + response.statusCode);
+                    }
+                    next();
+                });
+            }
+        })
+    });
 };
